jan-leila/sodium-fabric

made entity culling off by default

src/main/java/me/jellysquid/mods/sodium/client/gui/SodiumGameOptions.java
@@ -32,7 +32,7 @@ public class SodiumGameOptions {
         public boolean useChunkMultidraw = true;
 
         public boolean animateOnlyVisibleTextures = true;
-        public boolean useEntityCulling = true;
+        public boolean useEntityCulling = false;
         public boolean useParticleCulling = true;
         public boolean useFogOcclusion = true;
         public boolean useCompactVertexFormat = true;

tildejustin/mcsr-sodium

(cherry picked from commit 2704343e14f3f50b9e118fc1576ff7535d9eb40f)

src/main/java/me/jellysquid/mods/sodium/client/gui/SodiumGameOptions.java
@@ -32,7 +32,7 @@ public class SodiumGameOptions {
     public static class AdvancedSettings {
         public ChunkRendererBackendOption chunkRendererBackend = ChunkRendererBackendOption.BEST;
         public boolean animateOnlyVisibleTextures = true;
-        public boolean useAdvancedEntityCulling = true;
+        public boolean useAdvancedEntityCulling = false;
         public boolean useParticleCulling = true;
         public boolean useFogOcclusion = true;
         public boolean useCompactVertexFormat = true;