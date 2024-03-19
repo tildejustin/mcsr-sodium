jan-leila/sodium-fabric

new: Add option for planar fog

src/main/java/me/jellysquid/mods/sodium/client/gui/SodiumOptionsGUI.java
@@ -46,6 +46,7 @@ public class SodiumOptionsGUI extends Screen {
         this.pages.add(SodiumGameOptionPages.general());
         this.pages.add(SodiumGameOptionPages.quality());
         this.pages.add(SodiumGameOptionPages.advanced());
+        this.pages.add(SodiumGameOptionPages.unofficial());
     }
 
     public void setPage(OptionPage page) {

src/main/java/me/jellysquid/mods/sodium/mixin/features/options/MixinBackgroundRenderer.java
@@ -1,0 +1,20 @@
+package me.jellysquid.mods.sodium.mixin.features.options;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import me.jellysquid.mods.sodium.client.SodiumClientMod;
+import net.minecraft.client.render.BackgroundRenderer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(BackgroundRenderer.class)
+public abstract class MixinBackgroundRenderer {
+    @Redirect(method = "applyFog", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/RenderSystem;setupNvFogDistance()V"))
+    private static void redirectSetupNvFogDistance() {
+        if (SodiumClientMod.options().unofficial.usePlanarFog) {
+            return;
+        } else {
+            RenderSystem.setupNvFogDistance();
+        }
+    }
+}

tildejustin/mcsr-sodium

src/main/java/me/jellysquid/mods/sodium/client/gui/SodiumOptionsGUI.java
@@ -52,6 +52,7 @@ public class SodiumOptionsGUI extends Screen {
         this.pages.add(SodiumGameOptionPages.general());
         this.pages.add(SodiumGameOptionPages.quality());
         this.pages.add(SodiumGameOptionPages.advanced());
+        this.pages.add(SodiumGameOptionPages.speedrun());
     }
 
     public void setPage(OptionPage page) {

src/main/java/me/jellysquid/mods/sodium/mixin/features/options/MixinBackgroundRenderer.java
@@ -1,0 +1,20 @@
+package me.jellysquid.mods.sodium.mixin.features.options;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import me.jellysquid.mods.sodium.client.SodiumClientMod;
+import net.minecraft.client.render.BackgroundRenderer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(BackgroundRenderer.class)
+public abstract class MixinBackgroundRenderer {
+    @Redirect(method = "applyFog", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/RenderSystem;setupNvFogDistance()V"))
+    private static void redirectSetupNvFogDistance() {
+        if (SodiumClientMod.options().speedrun.usePlanarFog) {
+            return;
+        } else {
+            RenderSystem.setupNvFogDistance();
+        }
+    }
+}